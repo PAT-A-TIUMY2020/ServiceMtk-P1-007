44e6f08760f4934fc4abea3c3e94af78502ff925
istytsaniya <tsaniyaisty08@gmail.com>
2020-11-09 23:32:40 +0700

    add exception handler

--- ServiceMtk-P1-20180140007/IMatematika.cs
+++ ServiceMtk-P1-20180140007/IMatematika.cs
@@ -13,15 +13,20 @@ namespace ServiceMtk_P1_20180140007
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         /* output->*/
         int Tambah(int a, int b); //method
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
 
     }
@@ -43,4 +48,14 @@ namespace ServiceMtk_P1_20180140007
             set { _y = value; }
         }
     }
+
+    [DataContract]
+    public class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+
 }
